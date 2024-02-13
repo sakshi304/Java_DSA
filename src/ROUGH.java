635a5be4f6bbf91194cecaf8afa83ddd42eeaf4a
sakshi304 <91881840+sakshi304@users.noreply.github.com>
2024-02-13 12:34:34 +0530

    .idea : i dont have any idea

---
+++ src/ROUGH.java
@@ -0,0 +1,11 @@
+
+public class ROUGH {
+    public static void print(String src, String helper, String dest){
+        System.out.println(src+helper+dest);
+       // print(helper);
+
+    }
+    public static void main(String[] args) {
+        print("A","B","C");
+    }
+}
